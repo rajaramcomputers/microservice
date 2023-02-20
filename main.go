860e43e5a864f14acc3e0e3591af094a470d4514
rajaramcomputers <rajaramcomputers@gmail.com>
2023-02-20 19:50:41 +0400

    modified:   go.mod 	new file:   go.sum 	modified:   main.go

--- main.go
+++ main.go
@@ -8,31 +8,32 @@ import (
 	"os/signal"
 	"time"
 
+	"github.com/gorilla/mux"
 	"rajaramsystems.com/microservice/handlers/handlers"
 )
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	//hh := handlers.NewHello(l)
-	gh := handlers.NewGoodbye(l)
+	//gh := handlers.NewGoodbye(l)
 
 	//this handler was created by Dr Ram
-	homeh := handlers.NewHome(l)
-	ch := handlers.NewContact(l)
+	//homeh := handlers.NewHome(l)
+	//ch := handlers.NewContact(l)
 	//
 
 	//from the lecture with respect to product handler
 
 	ph := handlers.NewProducts(l)
-	goh := handlers.NewGoals(l)
+	//goh := handlers.NewGoals(l)
 
-	sm := http.NewServeMux()
+	sm := mux.NewRouter()
 	sm.Handle("/", ph)
-	sm.Handle("/goodbye", gh)
-	sm.Handle("/home", homeh)
-	sm.Handle("/contact", ch)
-	sm.Handle("/products", ph)
-	sm.Handle("/goals", goh)
+	//sm.Handle("/goodbye", gh)
+	//sm.Handle("/home", homeh)
+	//sm.Handle("/contact", ch)
+	//sm.Handle("/products", ph)
+	//sm.Handle("/goals", goh)
 	s := &http.Server{
 		Addr:         ":9090",
 		Handler:      sm,
